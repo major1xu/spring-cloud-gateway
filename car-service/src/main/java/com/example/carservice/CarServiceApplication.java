cfceb3160375dffba45b016a29d9c6a32a7d3094
Minjie Xu <Minjie.Xu@gmail.com>
2019-09-10 21:44:18 -0500

    Secure Gateway to Microservice Communication via jwt

--- car-service/src/main/java/com/example/carservice/CarServiceApplication.java
+++ car-service/src/main/java/com/example/carservice/CarServiceApplication.java
@@ -16,9 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.apigateway.Car;
-import com.example.apigateway.GetMapping;
-import com.example.apigateway.RestController;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
